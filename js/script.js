b42b21a046e5901236e0f5d97a84b706aeb2b378
Kécia Santos <keciakaroline@gmail.com>
2022-06-10 20:21:27 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 const choices = ["rock", "paper", "scissors"];
 const whoWins = { rock: "paper", paper: "scissors", scissors: "rock" };
 const capitalize = (word) => word.charAt(0).toUpperCase() + word.slice(1);
-let playerSelection = prompt("Choose: rock, paper or scissors");
 
 function computerPlay() {
   //result can be: Rock, Paper or Scissors
-  return choices[Math.floor(Math.random() * choices.length)]; //to get a whole number between the choices sizes
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
+  playerScore = 0;
+  computerScore = 0;
+  tieScore = 0;
   playerSelection = playerSelection.toLowerCase();
   const ifWin = (winner, loser) =>
     `You Win! ${capitalize(winner)} beats ${capitalize(loser)}`;
@@ -24,15 +26,22 @@ function playRound(playerSelection, computerSelection) {
     return ifLose(computerSelection, playerSelection);
   }
 }
-//console.log(playRound(playerSelection, computerSelection));
 
 function game() {
   for (let i = 1; i <= 5; i++) {
-    //const playerSelection = "Paper";
-    console.log("player plays: " + playerSelection);
-    const computerSelection = computerPlay();
-    console.log("computer plays: " + computerSelection);
-    console.log(playRound(playerSelection, computerSelection), i);
+    let playerSelection = prompt("Choose: rock, paper or scissors");
+    if (
+      playerSelection == "rock" ||
+      playerSelection == "paper" ||
+      playerSelection == "scissors"
+    ) {
+      console.log("player plays: " + playerSelection);
+      const computerSelection = computerPlay();
+      console.log("computer plays: " + computerSelection);
+      console.log(playRound(playerSelection, computerSelection), i);
+    } else {
+      alert("type a valid answer");
+    }
   }
 }
 
